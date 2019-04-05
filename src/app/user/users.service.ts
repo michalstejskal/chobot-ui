0f5fd429a6b2e571f9034687ec78ab0a60278326
mMichal Stejskal <stejsky.s@gmail.com>
2019-04-05 11:40:07 +0200

    fix call url hard code constant

--- src/app/user/users.service.ts
+++ src/app/user/users.service.ts
@@ -4,11 +4,11 @@ import {HandleError, HttpErrorHandler} from '../http-error-handler.service';
 import {Observable} from 'rxjs';
 import {User} from './model/user';
 import {catchError} from 'rxjs/operators';
+import {API_ROOT_DOMAIN} from '../shared/api-util';
 
 
 @Injectable()
 export class UsersService {
-  usersUrl = 'http://localhost:8090/api/v1/user/';
   private handleError: HandleError;
 
   constructor(private http: HttpClient, httpErrorHandler: HttpErrorHandler) {
@@ -17,21 +17,21 @@ export class UsersService {
 
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.usersUrl)
+    return this.http.get<User[]>(API_ROOT_DOMAIN + 'user')
       .pipe(
         catchError(this.handleError('getUsers', []))
       );
   }
 
   getUserMe(): Observable<User> {
-    return this.http.get<User>(this.usersUrl + 'me')
+    return this.http.get<User>(API_ROOT_DOMAIN + 'user/me')
       .pipe(
         catchError(this.handleError('getUser', null))
       );
   }
 
   putUser(user: User): Observable<User> {
-    return this.http.put(this.usersUrl, user)
+    return this.http.put(API_ROOT_DOMAIN + 'user', user)
       .pipe(
         catchError(this.handleError('putUser', null))
       );
